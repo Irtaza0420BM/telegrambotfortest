cd15156f807c8651801ee36f3cfa7fcb86238974
Muhummad Irtaza Ashar <m.irtazabinashar@gmail.com>
2024-12-06 21:06:27 +0500

    requestMade

--- controllers/lib/axios.js
+++ controllers/lib/axios.js
@@ -3,6 +3,7 @@ const base_url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/`
 
 
 const setWebhook = async () => {
+  console.log("Request made")
   console.log(process.env.VERCEL_URL)
   const webhookUrl = `https://${process.env.VERCEL_URL}/`; // Vercel's URL with an extra / to get result at / route :D 
   return (`${base_url}/setWebhook?url=${webhookUrl}`)
